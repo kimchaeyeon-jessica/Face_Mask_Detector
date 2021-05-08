61fc8cb630d81ed61e04b295316a38e13328ffd9
kimchaeyeon <jessicakcy0501@gmail.com>
2021-05-08 18:00:37 +0900

    face_landmark
    face_landmark를 이용하여 정교하게 마스크 리사이즈,배치함

--- image_paste.py
+++ image_paste.py
@@ -1,5 +1,7 @@
 import face_recognition
 from PIL import Image, ImageDraw
+import numpy
+import math
 
 image_path = 'data/ElonMusk.jpg'
 mask_image_path = 'data/mask.png'
@@ -9,7 +11,7 @@ face_image = Image.fromarray(face_image_np)
 draw = ImageDraw.Draw(face_image)
 
 mask_image = Image.open(mask_image_path)
-mask_image = mask_image.resize((right-left,bottom))  #마스크 사이즈를 리사이즈함
+mask_image = mask_image.resize((math.sqrt(math.pow(face_landmark['chin'][1][0] - face_landmark['chin'][15][0] , 2) + math.pow(face_landmark['chin'][1][1] - face_landmark['chin'][15][1] , 2) ),  math.sqrt(math.pow(face_landmark['nose_bridge'][1][0] - face_landmark['chin'][8][0] , 2) + math.pow(face_landmark['nose_bridge'][1][1] - face_landmark['chin'][8][1] , 2) )))  #마스크 사이즈를 리사이즈함
 
 face_image.paste(mask_image, (right-left,bottom), mask_image) #마스크 사진에서 마스크만 추출하고 나머지 영역을 투명으로 바꿔줌
 face_image.show()
---
+++ test_dictionary.py
@@ -0,0 +1,5 @@
+d={'A':10, 123:'B','10.34':[3,4,5],'C': [(1,2),(43,44)]} #key, value
+
+print(d['10.34']) #key
+#print(face_landmark['chin'][1][0])
+print(d['C'][1][0])
