3e58ddc6d96fa805dc9c8439c929c15c61faae65
kimchaeyeon <jessicakcy0501@gmail.com>
2021-05-22 15:06:28 +0900

    마스크 기울기에 맞춰 합성+리팩토링링

---
+++ lab_data/face_rotation.py
@@ -0,0 +1,31 @@
+import face_recognition
+import numpy as np
+from PIL import Image, ImageDraw
+
+face_image_path = '../data/without_mask/augmented_image_37.jpg'
+mask_image_path = '../data/mask.png'
+
+face_image_np = face_recognition.load_image_file(face_image_path)
+face_locations = face_recognition.face_locations(face_image_np)
+face_landmarks = face_recognition.face_landmarks(face_image_np,face_locations)
+
+face_landmark_image = Image.fromarray(face_image_np)
+draw = ImageDraw.Draw(face_landmark_image)
+for face_landmark in face_landmarks:
+    nb = face_landmark['nose_bridge']
+    nb_top = nb[0]
+    nb_bottom = nb[3]
+    dx = abs(nb_top[0] - nb_bottom[0])
+    dy = abs(nb_top[1] - nb_bottom[1])
+
+    face_radian = np.arctan2(dx,dy)
+    face_degree = np.rad2deg(face_radian)
+
+    mask_degree = 90 - face_degree
+
+    mask_image = mask_image.resize(80,50)
+    mask_image = mask_image.rotate(mask_degree)
+
+    face_landmark_image.paste(mask_image, (0,0), mask_image)
+
+face_landmark_image.show()
